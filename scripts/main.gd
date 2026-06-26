635663e4719a88b83d828af5a4376dd3fba7dccf
Nabil Babu <nabil.babu20@gmail.com>
2026-06-26 16:23:46 -0400

    Added basic 3D Character Controller, replaced Asset Store one in Spawner and main.gd player scene

--- scripts/main.gd
+++ scripts/main.gd
@@ -26,7 +26,7 @@ func _ready() -> void:
 		print("Steam did not initialize :(")
 
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().quit()
 
---
+++ scripts/player_3d.gd
@@ -0,0 +1,49 @@
+extends CharacterBody3D
+# Movement speeds
+const WALK_SPEED: float = 5.0
+const JUMP_VELOCITY: float = 4.5
+
+# Look sensitivity
+const MOUSE_SENSITIVITY: float = 0.003
+
+# Get the gravity from the project settings to be sync'd with RigidBody nodes.
+var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+@onready var head: Node3D = $Head
+@onready var camera: Camera3D = $Head/Camera3D
+
+func _ready():
+	
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event) -> void:
+	
+	if event is InputEventMouseMotion:
+		
+		rotate_y(-event.relative.x * MOUSE_SENSITIVITY)
+		
+		head.rotate_x(-event.relative.y * MOUSE_SENSITIVITY)
+		
+		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-89), deg_to_rad(89))
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	if Input.is_action_just_pressed("play_char_jump_action") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+
+	var input_dir: Vector2 = Input.get_vector("play_char_move_left_action", "play_char_move_right_action", "play_char_move_forward_action", "play_char_move_backward_action")
+	
+	var direction: Vector3 = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	if direction:
+		velocity.x = direction.x * WALK_SPEED
+		velocity.z = direction.z * WALK_SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, WALK_SPEED)
+		velocity.z = move_toward(velocity.z, 0, WALK_SPEED)
+
+	# Execute built-in Godot physics simulation and collision handling
+	move_and_slide()
