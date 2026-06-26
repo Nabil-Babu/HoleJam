909b47afc974a57806fe72b1e3e82fb065e4fbaf
Zed Poirier <49734422+zedpoirier@users.noreply.github.com>
2026-06-26 17:23:48 -0400

    Simplified Action Names

--- scripts/player_3d.gd
+++ scripts/player_3d.gd
@@ -38,11 +38,12 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	if Input.is_action_just_pressed("play_char_jump_action") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		
-	var input_dir: Vector2 = Input.get_vector("play_char_move_left_action", "play_char_move_right_action", "play_char_move_forward_action", "play_char_move_backward_action")
+	var input_dir: Vector2 = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction: Vector3 = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	print(input_dir)
 
 	if direction:
 		velocity.x = direction.x * WALK_SPEED
@@ -52,7 +53,7 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, WALK_SPEED)
 	
 	# Multiply velocity by sprint mult factor when sprinting
-	if Input.is_action_pressed("play_char_run_action"):
+	if Input.is_action_pressed("sprint"):
 		velocity.x *= SPRINT_MULT
 		velocity.z *= SPRINT_MULT
 		
@@ -60,5 +61,5 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	
 	# Grabbing logic
-	if Input.is_action_just_pressed("play_char_grab_action"):
+	if Input.is_action_just_pressed("grab"):
 		print("Im trying grab it")
