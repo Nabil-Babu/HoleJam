83e481a79e88da17f424f59df26d4e4ca9ce1866
Zed Poirier <49734422+zedpoirier@users.noreply.github.com>
2026-06-26 11:22:15 -0400

    more tests

--- scripts/main.gd
+++ scripts/main.gd
@@ -29,10 +29,10 @@ func _ready() -> void:
 func add_player(id : int = 1):
 	var player = player_scene.instantiate()
 	player.name = str(id)
-	players.add_child(player)
 	var posX = randf() * 500 + 100
 	var posY = randf() * 300 + 100
 	player.position = Vector2(posX, posY)
+	players.call_deferred("add_child", player)
 	lobby_ui.hide() # temp... maybe not best to leave this here
 	print("Player joined with ID: " + str(id))
 	print("Player spawned at location: X=" + str(posX) + ", Y=" + str(posY))
