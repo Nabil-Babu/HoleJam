1c1c7fa79e0a71dbd430dcc28bb43069fa9bc421
Zed Poirier <49734422+zedpoirier@users.noreply.github.com>
2026-06-28 19:17:16 -0400

    Added box Spawner

---
+++ scripts/box_spawner.gd
@@ -0,0 +1,28 @@
+extends Node3D
+
+@export var spawnTime : float = 1.0
+
+@onready var label: Label3D = $Visuals/Label3D/LabelBoxDelivery
+@onready var label2: Label3D = $Visuals/Label3D2/LabelBoxDelivery2
+
+var main_server
+var timer: float = 0.0
+var label_text: String = " BPS (Boxes Per Second)"
+
+func _ready() -> void:
+	main_server = get_parent().get_parent() # sloppy ref grabbing
+
+
+func _process(delta: float) -> void:
+	timer += delta
+	label.text = str(spawnTime) + label_text
+	label2.text = str(spawnTime) + label_text
+	if timer >= spawnTime:
+		spawn_a_box.rpc()
+		timer = 0.0
+
+
+@rpc("any_peer", "call_local", "reliable")
+func spawn_a_box():
+	print("spawning a boxx now <3")
+	main_server.spawn_box()
