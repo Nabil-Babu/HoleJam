dbcd91d60a57542270d9d9232ad6b18679f08f70
Zed Poirier <49734422+zedpoirier@users.noreply.github.com>
2026-06-28 09:53:39 -0400

    Update player_3d.gd

--- scripts/player_3d.gd
+++ scripts/player_3d.gd
@@ -81,15 +81,14 @@ func shove(force: Vector3):
 func _physics_process(delta: float) -> void:
 	if not is_multiplayer_authority():
 		return
+	animator.play("PlayerAnimations/Blob_Idle")
 	
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-	else: 
-		animator.play("PlayerAnimations/Blob_Idle")
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		animator.stop()
+		#animator.stop()
 		
 	var input_dir: Vector2 = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction: Vector3 = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
