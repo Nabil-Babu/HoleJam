7cc912677633e49a469f092d2acfd9b7117c2c3c
Zed Poirier <49734422+zedpoirier@users.noreply.github.com>
2026-06-28 19:34:36 -0400

    Update box_spawner.gd

--- scripts/box_spawner.gd
+++ scripts/box_spawner.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-@export var spawnTime : float = 1.0
+@export var spawnTime : float = 2.0
 
 @onready var label: Label3D = $Visuals/Label3D/LabelBoxDelivery
 @onready var label2: Label3D = $Visuals/Label3D2/LabelBoxDelivery2
@@ -16,10 +16,12 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if not multiplayer.is_server():
 		return
+	print("test this spawner")
 	timer += delta
 	label.text = str(spawnTime) + label_text
 	label2.text = str(spawnTime) + label_text
 	if timer >= spawnTime:
+		print("now we spawning")
 		spawn_a_box.rpc_id(1)
 		timer = 0.0
 
