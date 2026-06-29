ffdd2f199695994daa6fced7be8db7f6d0f956b5
Nabil Babu <nabil.babu20@gmail.com>
2026-06-28 20:14:48 -0400

    Added team name for holes and check for human in player_3d

--- scripts/hole_area.gd
+++ scripts/hole_area.gd
@@ -1,5 +1,7 @@
 extends Area3D
 
+@export var team_name: String
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
@@ -11,17 +13,19 @@ func _on_body_entered(body: Node3D) -> void:
 			body.set_collision_mask_value(4, false)
 			if !body.box_despawn.is_connected(_update_score.bind(body.boxScore)):
 				body.box_despawn.connect(_update_score.bind(body.boxScore))
-	if body is CharacterBody3D:
+	if body is CharacterBody3D && body.is_human:
 		body.set_collision_mask_value(4, false)
 
 
 func _on_body_exited(body: Node3D) -> void:
 	if body is RigidBody3D:
 		body.set_collision_mask_value(4, true)
-	if body is CharacterBody3D:
+	if body is CharacterBody3D && body.is_human:
 		body.set_collision_mask_value(4, true)
 		body.call_deferred("set_global_position", Vector3(0,0,0))
+		_update_score(-10)
+		
 
 
 func _update_score(points: int):
-	get_parent().get_parent().update_game_score(points)
+	get_parent().get_parent().update_team_score(team_name, points)
